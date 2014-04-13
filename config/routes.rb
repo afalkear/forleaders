268e15b4ab3d6193e6b2cefe1728570958626f19
Alex Falke <afalkear@gmail.com>
2014-04-13 07:19:41 -0300

    [#69403642] individual consultory static pages works with bootstrab tab

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Forleaders::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
 
+  resources :static_pages
+  get 'individual_consultory', to:'static_pages#individual_consultory'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
