8f2b80780cd68082abe67637a54c85c5dbf92869
Alex Falke <afalkear@gmail.com>
2014-04-20 12:44:40 -0300

    [#69406014] add an uploader via carrierwave

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ActiveRecord::Base
   has_and_belongs_to_many :categories
   attr_reader :category_tokens
 
+  mount_uploader :article_image, ArticleImageUploader
+
 
   def category_tokens=(tokens)
     self.categories << Category.get_from_tokens(tokens)
