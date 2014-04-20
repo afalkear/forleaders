fefa4e1d68669a58139ef50e9a9fa0f7ec38af23
Alex Falke <afalkear@gmail.com>
2014-04-19 23:19:00 -0300

    [#69129996] add autocomplete category for article

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,10 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.order(:name)
+    respond_to do |format|
+      format.html
+      format.json { render json: @categories.tokens(params[:q]) }
+    end
   end
 
   def show
@@ -13,7 +18,7 @@ class CategoriesController < ApplicationController
     @category = Category.new
     @category.name = params[:category][:name]
     if @category.save
-      redirect_to category_path(@category.id)
+      redirect_to category_path(@category.id), notice: "Successfully created category."
     else
       flash[:alert] = "Error"
       render action: 'new'
