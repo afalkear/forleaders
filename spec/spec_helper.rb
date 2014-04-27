fb1a73c2778c0c2e3c637fc648cf7f4482a0d2ba
Alex Falke <afalkear@gmail.com>
2014-04-26 21:36:14 -0300

    [#70086690] added spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,27 +4,13 @@ require 'rspec/rails'
 
 RSpec.configure do |config|
 
-  # RSpec automatically cleans stuff out of backtraces;
-  # sometimes this is annoying when trying to debug something e.g. a gem
-  puts "                      ATTENTION: Backtraces are scoped to app code only."
-  puts "                                 Edit spec_helper to debug all code"
-  config.backtrace_clean_patterns = [
-      /\/lib\d*\/ruby\//,
-      /bin\//,
-      /gems/,
-      /spec\/spec_helper\.rb/,
-      /lib\/rspec\/(core|expectations|matchers|mocks)/
-  ]
-
-  config.mock_with :rspec
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
 
   # Only run specs marked with :focus in metadata or all specs if none with :focus is found.
-  config.treat_symbols_as_metadata_keys_with_true_values = true
+  # config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
 
@@ -32,7 +18,7 @@ RSpec.configure do |config|
     # in spec/support/ and its subdirectories.
     Dir["./spec/support/**/*.rb"].each {|f| require f}
   
-  config.include Devise::TestHelpers, :type => :controller
+  # config.include Devise::TestHelpers, :type => :controller
 
   config.before(:each) do
     
@@ -44,5 +30,5 @@ RSpec.configure do |config|
 
 
   # Allows us to use build(...) instead of FactoryGirl.build(...)
-  config.include FactoryGirl::Syntax::Methods
+  # config.include FactoryGirl::Syntax::Methods
 end
