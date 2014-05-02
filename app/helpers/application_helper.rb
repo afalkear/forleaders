ac19317dd33e6781a06067fce650d32b37f4899e
Alex Falke <afalkear@gmail.com>
2014-05-02 19:21:26 -0300

    [#70441462][#70441462] add asterisk to en.yml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def title(page_title)
     content_for :title, page_title.to_s
   end
+
+  def mark_required(object, attribute)  
+    "*" if object.class.validators_on(attribute).map(&:class).include? ActiveModel::Validations::PresenceValidator  
+  end 
 end
