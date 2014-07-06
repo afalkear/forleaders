7b4804844d81e29c26776276fc4a7f8c3e14161c
Alex Falke <afalkear@gmail.com>
2014-07-06 00:57:04 -0300

    some tweaks for better devise layout, that just didn't work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  after_filter :store_location
+  layout :layout_by_resource
 
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
@@ -12,9 +14,34 @@ class ApplicationController < ActionController::Base
     @current_ability ||= Ability.new(current_user)
   end
 
+
+  # Redirect to page where the user was visiting
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    return unless request.get? 
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath 
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
   protected
 
   # def after_sign_in_path_for(resource)
   #   redirect_to articles_url
   # end
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
 end
