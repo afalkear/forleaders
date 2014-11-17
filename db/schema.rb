317a221a91961db628e186120f7ae0c26e2f8104
Alex Falke <afalkear@gmail.com>
2014-11-17 17:38:54 -0300

    remove config

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140822191518943294) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "admins", force: true do |t|
     t.string   "email"
     t.string   "encrypted_password"
