e7692c50c15a6d1d12ea98dfbbddba7a1db247bd
Alex Falke <afalkear@gmail.com>
2014-11-18 00:57:35 -0300

    fixed article index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,14 +5,16 @@ class ArticlesController < ApplicationController
 
   def index
     category = params[:category].present? ? params[:category] : ""
-      
-    @featured_articles = Article.includes(:categories).where("categories.name LIKE ? AND lang = ? AND publish_at <= ?", category, params[:locale], DateTime.now).references(:categories).take(4)
+    
+    puts ""
+    puts "category: #{category}"
+    @featured_articles = Article.includes(:categories).where("categories.name LIKE ? AND lang = ? AND publish_at <= ?", "%#{category}%", params[:locale], DateTime.now).references(:categories).take(4)
     @latest_articles = Article.includes(:categories).where("publish_at <= ? AND lang = ?", DateTime.now, params[:locale]).last(3)
   end
 
   def show
     @article = Article.find(params[:id])
-    @related_articles =  Article.includes(:categories).where("categories.name LIKE ? AND lang = ? AND publish_at <= ?", @article.category, params[:locale], DateTime.now).references(:categories).where.not(id: @article.id).take(3) # Article.page(params[:page]).per(3)
+    @related_articles =  Article.includes(:categories).where("categories.name LIKE ? AND lang = ? AND publish_at <= ?", "%#{@article.category}%", params[:locale], DateTime.now).references(:categories).where.not(id: @article.id).take(3) # Article.page(params[:page]).per(3)
     @latest_articles = Article.includes(:categories).where("publish_at <= ?", DateTime.now).where.not(id: @article.id).last(3) #Article.page(params[:page]).per(3).order('created_at DESC')
     youtube_url = @article.video_url
     youtube_id = nil
