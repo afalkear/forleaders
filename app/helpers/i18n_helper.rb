fb09ec155560ce5b36ee3ee8326e6440a53e50ba
Alex Falke <afalkear@gmail.com>
2016-09-12 20:34:08 -0300

    [#humanize]

--- app/helpers/i18n_helper.rb
+++ app/helpers/i18n_helper.rb
@@ -2,7 +2,7 @@ module I18nHelper
   def translate(key, options={})
     super(key, options.merge(raise: true))
   rescue I18n::MissingTranslationData
-    key.split('.')[-1]
+    key.split('.')[-1].humanize
   end
   alias :t :translate
 end
