65f2e319bc6212d674636b45c403b0dbff88309a
Alex Falke <afalkear@gmail.com>
2017-04-25 14:12:25 -0300

    [#142982625] quick fix for the images

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,18 +7,18 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-  storage :dropbox
+  storage :file
+  # storage :dropbox
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "Public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{Rails.root.to_s}/public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def url
-    "https://dl.dropboxusercontent.com/u/1827912/#{path[7..-1]}" if path
-  end
+  #def url
+  #  "https://dl.dropboxusercontent.com/u/1827912/#{path[7..-1]}" if path
+  #end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
